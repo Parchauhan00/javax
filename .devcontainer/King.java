27d4eeaf48f0c18f0478c4e3956e3182cc936556
Prashant Chauhan <vipchauhan00@gmail.com>
2023-04-15 02:07:24 +0000

    container config done

---
+++ .devcontainer/King.java
@@ -0,0 +1,6 @@
+public class King {
+    public static void main(String arg []) {
+        System.out.println("Java is Running");
+    }
+
+}
---
+++ .devcontainer/pc/Testing.java
@@ -0,0 +1,5 @@
+class Testing {
+    public static void main(String arg []) {
+        System.out.println("Java is Running");
+    }
+}
